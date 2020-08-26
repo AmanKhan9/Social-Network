f645cd2e9d183741852197954dac04edd2b3294b
Aman Khan <aman0786khan@gmail.com>
2020-08-27 00:39:34 +0530

    Post like and unlike routees

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -95,4 +95,52 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+//@route    PUT api/posts/like/:id
+//@desc     Like a post
+//@acess    Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//@route    PUT api/posts/unlike/:id
+//@desc     Unlike a post
+//@acess    Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post not yet liked" });
+    }
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
