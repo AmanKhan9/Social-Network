7924b30cd0780381f650b25a148887bee8675ad3
Aman Khan <aman0786khan@gmail.com>
2020-09-02 00:50:39 +0530

    Add post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,6 +5,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from "../actions/types";
 
 export const getPosts = () => async (dispatch) => {
@@ -77,6 +78,30 @@ export const deletePost = (id) => async (dispatch) => {
       },
     });
     dispatch(setAlert("Post Removed", "success"));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+export const addPost = (formData) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    const res = await axios.post("/api/posts/", formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+    dispatch(setAlert("Post Created", "success"));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
